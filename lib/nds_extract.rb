17b021b1ef754837d713d645f738ea2c13723067
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-13 19:04:09 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,6 +9,7 @@ def gross_for_director(director_data)
     totals[director_name] += nds[director_index][:movies][movie_index][movie_index][:worldwide_grosses]
     movie_index += 1
   end
+  director_index += 1
 end
 
 # Write a method that, given an NDS creates a new Hash
@@ -16,6 +17,13 @@ end
 #
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
+  director_index = 0
   result = {}
-  nil
+  while director_index < nds.length do
+    director_name = nds[director_index][:name]
+    totals[director_name] = 0
+    gross_for_director
+  end
+  results.push(gross_for_director)
+  
 end
